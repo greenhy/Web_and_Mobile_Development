590ded834ae76a6ed8a81194b3f71473de50ea37
greenhy <1yougsll@gmail.com>
2021-03-22 13:37:08 +1000

    delete assginment

--- ass1/average.js
+++
@@ -1,5 +0,0 @@
-function average(x, y) {
-    return (x+y)/2;
-}
-
-// average(-6,67);
--- ass1/compare.js
+++
@@ -1,6 +0,0 @@
-function compare(x, y) {
-    return x===y;
-}
-
-// console.log(compare(1,2));
-// console.log(compare(2,2));
