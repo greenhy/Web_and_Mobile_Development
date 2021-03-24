352ec7953ef3ff5cba0ec1157a8d55455c227912
greenhy <1yougsll@gmail.com>
2021-03-24 17:29:35 +1000

    headline functiln

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -104,14 +104,14 @@ const AnimalComponet = ({name, number, eats}) =>
 
 function App() {
   // using hook
-  const [count, setCount] = useState(0);
-  const increment = ()=>{
-    setCount((oldCount)=> oldCount+1);
-  }
+  // const [count, setCount] = useState(0);
+  // const increment = ()=>{
+  //   setCount((oldCount)=> oldCount+1);
+  // }
 
-  const decrement = ()=>{
-    setCount((oldCount)=> oldCount-1);
-  }
+  // const decrement = ()=>{
+  //   setCount((oldCount)=> oldCount-1);
+  // }
 
   return (
     <div className="App">
@@ -136,10 +136,43 @@ function App() {
       </header> */}
 
       {/* Hook example */}
-      <h1>Like Counter</h1>
+      {/* <h1>Like Counter</h1>
+      <p>Overall Count: {count} </p>
+      <button onClick ={increment}>Like</button>
+      <button onClick ={decrement}>Dislike</button> */}
+      {/* <LikeCounter/> */}
+      <HeadDline title="Carrot"/>
+      <HeadDline title="Banana"/>
+      <HeadDline title="Apple"/>
+    </div>
+  );
+}
+
+// make function for like, dislike btns
+function LikeCounter(){
+  const [count, setCount] = useState(0);
+  const increment = ()=>{
+    setCount((oldCount)=> oldCount+1);
+  }
+
+  const decrement = ()=>{
+    setCount((oldCount)=> oldCount-1);
+  }
+  return(
+    <div>
+      {/* <h1>Like Counter</h1> */}
       <p>Overall Count: {count} </p>
       <button onClick ={increment}>Like</button>
       <button onClick ={decrement}>Dislike</button>
+    </div>
+  )
+}
+
+function HeadDline({title}) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <LikeCounter/>
     </div>
   );
 }
